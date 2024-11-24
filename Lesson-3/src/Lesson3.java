615130c0010c342dca433a5fa23cf171c7851b3f
SabrynaElkind <134516918+SabrynaElkind@users.noreply.github.com>
2024-11-24 16:38:40 +0100

    Update Lesson-3/src/Lesson3.java
    Co-authored-by: Dobrynin Maksym <dobmax@users.noreply.github.com>

--- Lesson-3/src/Lesson3.java
+++ Lesson-3/src/Lesson3.java
@@ -6,7 +6,7 @@ public class Lesson3 {
 
     public static void checkSumSign (int a, int b){
 
-        if ((a+b) >= 0){
+        if (a+b >= 0){
             System.out.println("Сумма положительная");
         } else {
             System.out.println("Сумма отрицательная");
