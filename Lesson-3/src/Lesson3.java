5718d6c12a706d507739e0bedfab00872af38b5d
SabrynaElkind <134516918+SabrynaElkind@users.noreply.github.com>
2024-11-24 16:39:11 +0100

    Update Lesson-3/src/Lesson3.java
    Co-authored-by: Dobrynin Maksym <dobmax@users.noreply.github.com>

--- Lesson-3/src/Lesson3.java
+++ Lesson-3/src/Lesson3.java
@@ -34,12 +34,7 @@ public class Lesson3 {
     }
 
     public static boolean sum (int a, int b){
-        int sum = a+b;
-        if (sum>=10 & sum <=20){
-            return true;
-        } else {
-            return false;
-        }
+        return sum>=10 & sum <=20;
     }
 
 
