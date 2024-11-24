61f7396b71e9daca6601a8f4f7976a8a4e6d17e1
SabrynaElkind <134516918+SabrynaElkind@users.noreply.github.com>
2024-11-24 16:38:29 +0100

    Update Lesson-3/src/Lesson3.java
    Co-authored-by: Dobrynin Maksym <dobmax@users.noreply.github.com>

--- Lesson-3/src/Lesson3.java
+++ Lesson-3/src/Lesson3.java
@@ -17,7 +17,7 @@ public class Lesson3 {
 
         if (value <=0){
             System.out.println("Красный");
-        } else if (value > 0 & value <=100 ) {
+        } else if (value <=100 ) {
             System.out.println("Желтый");
 
         } else if (value > 100) {
